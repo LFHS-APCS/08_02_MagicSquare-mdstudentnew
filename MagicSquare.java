f4292cd78154c8ab01dbff1cd5a68a6e856d0771
mdstudentnew <74984449+mdstudentnew@users.noreply.github.com>
2021-02-24 09:10:31 -0600

    Update MagicSquare.java

--- MagicSquare.java
+++ MagicSquare.java
@@ -25,8 +25,63 @@ public class MagicSquare
      */
     public boolean isMagicSquare()
     {
-        return false;   // complete this method
+      int magicConstant = addRow(0);
+        if(array.length == 0 || array.length != array[0].length)
+            return false;
+        
+            for(int i = 1; i < array.length; i++){
+                int r = addRow(i);
+                if(r != magicConstant)
+                    return false;
+            }
+            
+            for(int i = 0; i<array.length; i++){
+                int c = addCol(i);
+                if(c != magicConstant)
+                    return false;
+            }
+            
+            if(magicConstant != addDiagonalTopLeft() || magicConstant != addDiagonalBottomLeft() || addDiagonalTopLeft() != addDiagonalBottomLeft())
+                return false;
+            
+            return true;
     }
+
+
+     public int addRow(int row){
+        int sum1 = 0;
+        for(int i=0; i<array[0].length;i++){
+            sum1 += array[row][i];
+        }
+        return sum1;
+    }
+    
+      public int addCol(int col){
+        int sum2 = 0;
+        for(int i=0; i<array.length;i++){
+            sum2 += array[i][col];
+        }
+        return sum2;
+    }
+    
+    public int addDiagonalTopLeft(){
+        int sum3 = 0;
+        for(int i = 0; i<array.length; i++){
+             sum3 += array[i][i];
+        }
+        return sum3;
+    }
+    
+    public int addDiagonalBottomLeft(){
+        int sum4 = 0;
+        for(int i = 0; i< array.length; i++){
+            int j = array.length - 1 - i;
+            sum4 += array[i][j];
+        }
+        return sum4;
+        
+    }
+
 
     /**
      * Returns a String which represents the array.
